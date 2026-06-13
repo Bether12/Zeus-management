75ad02844be9b8125334454f83ac4e28a97a2b0a
Bether12 <elnenogomez13@gmail.com>
2026-06-13 19:53:10 -0400

    Add initialization and querying functions to main.js and add initial elements to index.html

--- src/main.js
+++ src/main.js
@@ -1,18 +1,46 @@
-const { invoke } = window.__TAURI__.core;
+import { tauri } from "@tauri-apps/api";
 
-let greetInputEl;
-let greetMsgEl;
+async function initializeDatabase(){
+  try{
+    //Creating/loading database file
+    const db = await tauri.invoke('plugin:sql|load', { db: 'sqlite:gym.db'});
+    
+    //Define users structure with idempotency
+    const usersScheme = `
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        inscription_date TEXT DEFAULT CURRENT_TIMEStAMP,
+        expiration_date TEXT NOT NULL,
+        paid_status TEXT DEFAULT 'Active'
+      );`;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+      //Define payments structure with idempotency
+      const paymentScheme = `
+      CREATE IF NOT EXISTS payment_record(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        amount_paid INTEGER NOT NULL,
+        payment_date TEXT DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+      );`;
+
+      //Execute scheme creation
+    await tauri.invoke('plugin:sql|execute', {query: usersScheme, binds: []});
+    await tauri.invoke('plugin:sql|execute', {query: paymentScheme, binds: []});
+
+    console.log('Database verified and in OK status');
+  }catch(error){
+    console.log('Error while loading the database:', error);
+  }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+async function queryDatabase (query, params = []){
+  try{
+    return await tauri.invoke('plugin:sql|execute', { query: query, binds: params });
+  }catch(error){
+    console.log('Error detected:', error);
+
+    //Implement user error notification
+  }
+}

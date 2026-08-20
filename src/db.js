a0832419d13239ebda7650d7a6c221a22a5f1d6c
Bether12 <elnenogomez13@gmail.com>
2026-08-20 18:34:21 -0400

    Change changePaymentUser to update correctly new and old users' last_amount_payment field

--- src/db.js
+++ src/db.js
@@ -112,6 +112,16 @@ export class Data{
                 `UPDATE users_id SET last_payment = $1 WHERE id = $2`,
                 [oldUserPayDate[0].payment_date, userId]);
 
+            //Update the last amount paid of the old user
+            await this.queryDatabase('set', 
+                `UPDATE users_id SET last_amount_paid = COALESCE((SELECT amount_paid FROM payment_records WHERE payment_date = (SELECT last_payment FROM users_id WHERE id = $1)), 0) WHERE id = $1`, 
+                [userId]);
+
+            //Update the last amount paid of the new user
+            await this.queryDatabase('set', 
+                `UPDATE users_id SET last_amount_paid = COALESCE((SELECT amount_paid FROM payment_records WHERE payment_date = (SELECT last_payment FROM users_id WHERE id = $1)), 0) WHERE id = $1`, 
+                [newUserId]);
+
             await this.queryDatabase('set', `COMMIT`);
         } catch (error) {
             await this.queryDatabase('set', `ROLLBACK`);

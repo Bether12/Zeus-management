cbb1b3a0706341eae527a880aca26a2e91e771b0
Bether12 <elnenogomez13@gmail.com>
2026-09-05 07:36:26 -0400

    Make the date input have the current date value by default in renderPaymentForm

--- src/gui.js
+++ src/gui.js
@@ -248,6 +248,16 @@ export const GUI = function(Data, Event){
 
             const paymentDateInput = document.createElement('input');
             paymentDateInput.id = 'payment-date-input';
+
+            //Actual date calculation and setting
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = String(now.getMonth() + 1).padStart(2, '0');
+            const day = String(now.getDate()).padStart(2, '0');
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            paymentDateInput.value = `${year}-${month}-${day}T${hours}:${minutes}`;
+            
             paymentDateInput.required = true;
             paymentDateInput.type = 'datetime-local';
             form.appendChild(paymentDateInput);
